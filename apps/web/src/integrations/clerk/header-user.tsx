cfc58278e1f98b56a1b9573bb1f26b8073cb202a
Jacek Smolak <jacek.smolak@gmail.com>
2025-09-10 15:46:40 +0200

    Initial commit

---
+++ apps/web/src/integrations/clerk/header-user.tsx
@@ -0,0 +1,14 @@
+import { SignedIn, SignInButton, SignedOut, UserButton } from "@clerk/clerk-react";
+
+export default function HeaderUser() {
+	return (
+		<>
+			<SignedIn>
+				<UserButton />
+			</SignedIn>
+			<SignedOut>
+				<SignInButton />
+			</SignedOut>
+		</>
+	);
+}
---
+++ apps/web/src/orpc/schema.ts
@@ -0,0 +1,6 @@
+import { z } from "zod";
+
+export const TodoSchema = z.object({
+	id: z.int().min(1),
+	name: z.string(),
+});
