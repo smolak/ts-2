c0e12eba395a9cf58b7aa3efcc41f3080c06ef72
Jacek Smolak <jacek.smolak@gmail.com>
2025-09-24 21:49:58 +0200

    Register query client provider

--- apps/browser-extension/src/entrypoints/popup/main.tsx
+++ apps/browser-extension/src/entrypoints/popup/main.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
@@ -8,8 +9,12 @@ if (!rootElement) {
   throw new Error("Root element not found");
 }
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
