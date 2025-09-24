e7df1e99f62dc16f8b00dc6ae9b269760dbb658d
Jacek Smolak <jacek.smolak@gmail.com>
2025-09-24 21:34:39 +0200

    Public folder needs to be in the root path

--- apps/browser-extension/src/entrypoints/popup/App.tsx
+++ apps/browser-extension/src/entrypoints/popup/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import reactLogo from "@/assets/react.svg";
+import wxtLogo from "/wxt.svg";
 import "./App.css";
 
 function App() {
@@ -8,6 +9,9 @@ function App() {
   return (
     <>
       <div>
+        <a href="https://wxt.dev" rel="noopener" target="_blank">
+          <img src={wxtLogo} className="logo" alt="WXT logo" />
+        </a>
         <a href="https://react.dev" rel="noopener" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
